0b64d00efb28b9848f76f7352952434fdea9944f
Carlos Ochoa <uligibson@hotmail.com>
2024-02-08 22:09:10 -0600

    Update Babel configuration presets

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,18 @@
+// babel.config.js
+
+// This message will be logged when the file is read (at build time)
+console.log('Babel config is being read')
+
 module.exports = {
-    presets: [
-      ['@babel/preset-env', {targets: {node: 'current'}}],
-      "@babel/preset-react",
-      "@babel/preset-typescript"
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        // Targets the last 6 versions of all browsers and browsers used by > 0.25% of the global population
+        targets: '> 0.25%, last 6 versions, not dead',
+      },
     ],
-};
+    '@babel/preset-react', // For JSX transformation
+    '@babel/preset-typescript', // For TypeScript support
+  ],
+}
