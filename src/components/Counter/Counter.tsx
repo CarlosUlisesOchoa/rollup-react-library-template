a48201b3c5d8416083f731b49eaefaa4621fddfe
Carlos Ochoa <uligibson@hotmail.com>
2024-02-08 22:14:15 -0600

    refactor Counter component

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,23 +1,19 @@
 import { useState } from 'react'
-import './Counter.scss'
+import styles from './Counter.module.css'
 
 export type CounterProps = {
   initialCount?: number
 }
 
-import React from 'react'
-
-export function Counter({ initialCount = 0 }: CounterProps): JSX.Element {
+export const Counter = ({ initialCount = 0 }: CounterProps): JSX.Element => {
   //
   const [count, setCount] = useState(initialCount)
 
   return (
-    <div className='counter'>
+    <div className={styles.counter}>
       <button onClick={() => setCount(count - 1)}>-</button>
       <span>{count}</span>
       <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   )
 }
-
-export default Counter
